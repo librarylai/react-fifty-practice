4211dbe7c4769df80011d0e52c0b337bf446fe4e
librarylai <library@intella.co>
2021-09-12 19:06:19 +0800

    refactor: 處理專案設定檔，刪除不必要文件 new file:   craco.config.js modified:   package.json deleted:    src/App.css deleted:    src/App.test.tsx modified:   src/App.tsx modified:   src/index.css deleted:    src/logo.svg modified:   tsconfig.json new file:   tsconfig.paths.json modified:   yarn.lock
    feat: 增加 ExpandingCards 練習
    new file:   src/Containers/ExpandingCards/Component/Panel.tsx
    new file:   src/Containers/ExpandingCards/ExpandingCards.tsx

---
+++ src/Containers/ExpandingCards/Component/Panel.tsx
@@ -0,0 +1,66 @@
+import React, { FunctionComponent } from 'react'
+import styled, { css } from 'styled-components'
+// -- styled component props types ----------------------------------------------------------------- //
+export interface PanelProps {
+  imgUrl?: String
+  isOpen: boolean
+}
+export interface PanelComponentPropsType {
+  data: {
+    name: string
+    url: string
+  }[]
+  handlePanelClick(index: number): void
+  openIndex: number
+}
+
+const PanelContainer = styled.div`
+  display: flex;
+  width: 90vw;
+`
+const StyledPanel = styled.div<PanelProps>`
+  flex: ${({ isOpen }) => (isOpen ? 10 : 0.5)};
+  height: 80vh;
+  border-radius: 50px;
+  background-size: cover;
+  background-position: center;
+  margin: 10px;
+  position: relative;
+  transition: flex 0.5s ease-in;
+  cursor: pointer;
+  background-image: ${({ imgUrl }) => `url('${imgUrl}')`};
+`
+const panelActive = css`
+  transition: 0.3s ease-in-out;
+  transition-delay: 0.5s;
+`
+const PanelFont = styled.p<PanelProps>`
+  color: #fff;
+  text-shadow: 0px 0px 10px #999;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  opacity: ${({ isOpen }) => (isOpen ? 1 : 0)};
+  font-size: 30px;
+  ${({ isOpen }) => {
+    if (isOpen) return panelActive
+  }}
+`
+const Panel = ({ data, handlePanelClick, openIndex }: PanelComponentPropsType) => {
+  function renderPanel() {
+    return data.map((item, index) => {
+      let isOpen = openIndex === index
+      return (
+        <StyledPanel isOpen={isOpen} onClick={() => handlePanelClick(index)} imgUrl={item.url}>
+          <PanelFont isOpen={isOpen}>{item.name}</PanelFont>
+        </StyledPanel>
+      )
+    })
+  }
+  return <PanelContainer>{renderPanel()}</PanelContainer>
+}
+
+Panel.propTypes = {}
+
+export default Panel
