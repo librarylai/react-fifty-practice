394b64860f8646e89940e27a1a5f367de17eab36
librarylai <library@intella.co>
2021-12-01 00:38:57 +0800

    feat: 增加 React.lazy 和 Dynamic Import 和 Suspense src/App.tsx
    feat; 增加  Loadable Components 相關套件與設定
    .babelrc.json
    package.json
    yarn.lock

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import BlurryLoadingPage from './containers/blurryLoading/BlurryLoadingPage'
-import ExpandingCardsPage from './containers/expandingCards/ExpandingCardsPage'
-import React from 'react'
-import ScrollAnimationPage from './containers/scrollAnimation/ScrollAnimationPage'
-import RotatingNavigationPage from './containers/rotatingNavigation/RotatingNavigationPage'
-import HiddenSearchWidgetPage from './containers/hiddenSearchWidget/HiddenSearchWidgetPage'
-// import StepsPage from './containers/steps/StepsPage'
+import React, { Suspense } from 'react'
+/* 最原始版本 一般引入 */
+// import BlurryLoadingPage from '@/containers/blurryLoading/BlurryLoadingPage'
+// import ExpandingCardsPage from '@/containers/expandingCards/ExpandingCardsPage'
+// import ScrollAnimationPage from '@/containers/scrollAnimation/ScrollAnimationPage'
+// import RotatingNavigationPage from '@/containers/rotatingNavigation/RotatingNavigationPage'
+// import HiddenSearchWidgetPage from '@/containers/hiddenSearchWidget/HiddenSearchWidgetPage'
+// import StepsPage from '@/containers/steps/StepsPage'
 import {
   Routes, // 這邊需注意 官方文件寫的 Switch 已經在最新版本被改用為 Routes
   Route,
   Link,
 } from 'react-router-dom'
+/* 使用 React.lazy  */
+const BlurryLoadingPage = React.lazy(() => import(/*webpackChunkName:'BlurryLoadingPage'*/ '@/containers/blurryLoading/BlurryLoadingPage'))
+const ExpandingCardsPage = React.lazy(() => import(/*webpackChunkName:'ExpandingCardsPage'*/ '@/containers/expandingCards/ExpandingCardsPage'))
+const ScrollAnimationPage = React.lazy(() => import(/*webpackChunkName:'ScrollAnimationPage'*/ '@/containers/scrollAnimation/ScrollAnimationPage'))
+const RotatingNavigationPage = React.lazy(() => import(/*webpackChunkName:'RotatingNavigationPage'*/ '@/containers/rotatingNavigation/RotatingNavigationPage'))
+const HiddenSearchWidgetPage = React.lazy(() => import(/*webpackChunkName:'HiddenSearchWidgetPage'*/ '@/containers/hiddenSearchWidget/HiddenSearchWidgetPage'))
+// const StepsPage = React.lazy(() => import('/*webpackChunkName:'StepsPage'*/@/containers/steps/StepsPage'));
 
 function App() {
   return (
@@ -36,15 +44,17 @@ function App() {
           </li>
         </ul>
       </nav>
-      <Routes>
-        <Route path='/' element={<ExpandingCardsPage />} />
-        <Route path='/ExpandingCards' element={<ExpandingCardsPage />} />
-        <Route path='/ScrollAnimation' element={<ScrollAnimationPage />} />
-        <Route path='/RotatingNavigation' element={<RotatingNavigationPage />} />
-        <Route path='/BlurryLoading' element={<BlurryLoadingPage />} />
-        <Route path='/HiddenSearchWidget' element={<HiddenSearchWidgetPage />} />
-        {/* <Route path='/StepsPage' element={<StepsPage />} /> */}
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<ExpandingCardsPage />} />
+          <Route path='/ExpandingCards' element={<ExpandingCardsPage />} />
+          <Route path='/ScrollAnimation' element={<ScrollAnimationPage />} />
+          <Route path='/RotatingNavigation' element={<RotatingNavigationPage />} />
+          <Route path='/BlurryLoading' element={<BlurryLoadingPage />} />
+          <Route path='/HiddenSearchWidget' element={<HiddenSearchWidgetPage />} />
+          {/* <Route path='/StepsPage' element={<StepsPage />} /> */}
+        </Routes>
+      </Suspense>
     </div>
   )
 }
