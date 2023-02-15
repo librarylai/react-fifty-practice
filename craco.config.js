4aafb22e0dfeed05825b27bd1dda929ddfe62776
librarylai <library0515@gmail.com>
2023-02-15 12:08:51 +0800

    feat: add webpack-bundle-analyzer plugin

--- craco.config.js
+++ craco.config.js
@@ -1,23 +1,24 @@
 const CracoAlias = require('craco-alias')
 const LoadablePlugin = require('@loadable/webpack-plugin')
-
+const WebpackBundleAnalyzer = require('webpack-bundle-analyzer')
+const BundleAnalyzerPlugin = WebpackBundleAnalyzer.BundleAnalyzerPlugin
 module.exports = {
-	plugins: [
-		{
-			plugin: CracoAlias,
-			options: {
-				source: 'tsconfig',
-				// baseUrl SHOULD be specified
-				// plugin does not take it from tsconfig
-				baseUrl: '.',
-				/* tsConfigPath should point to the file where "baseUrl" and "paths" 
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        // baseUrl SHOULD be specified
+        // plugin does not take it from tsconfig
+        baseUrl: '.',
+        /* tsConfigPath should point to the file where "baseUrl" and "paths" 
              are specified*/
-				tsConfigPath: './tsconfig.paths.json',
-			},
-		},
-	],
-	webpack: {
-		// loadable component 套件，讓打包時編譯出 loadable-stats.json file 之後給 Server-Side Rending 使用
-		plugins: [new LoadablePlugin()],
-	},
+        tsConfigPath: './tsconfig.paths.json',
+      },
+    },
+  ],
+  webpack: {
+    // loadable component 套件，讓打包時編譯出 loadable-stats.json file 之後給 Server-Side Rending 使用
+    plugins: [new LoadablePlugin(), new BundleAnalyzerPlugin()],
+  },
 }
